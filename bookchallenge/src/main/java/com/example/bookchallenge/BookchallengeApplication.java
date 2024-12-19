883ab510647954f630181326200a95983ca70ba8
ruanvalente <ruanopensuselinux@gmail.com>
2024-12-18 21:36:59 -0300

    first commit

---
+++ bookchallenge/src/main/java/com/example/bookchallenge/BookchallengeApplication.java
@@ -0,0 +1,12 @@
+package com.example.bookchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookchallengeApplication.class, args);
+	}
+}
